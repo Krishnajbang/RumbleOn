088f6b48450fff4d138b831ca7606190a8d0dda0
Krish <Krish@ASUS>
2017-12-12 23:49:33 +0530

    commit 12/12

--- com.QADeploy/src/main/java/Application/App.java
+++ com.QADeploy/src/main/java/Application/App.java
@@ -12,6 +12,7 @@ import org.apache.log4j.xml.DOMConfigurator;
 import org.apache.xalan.xsltc.DOM;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -23,92 +24,79 @@ import org.w3c.dom.DOMConfiguration;
 
 
 
+
+
 import Utilities.*;
 import PageObjects.*;
+
 
 
 public class App extends UtilCommon{
 
 	ExcelFunctionality data = new ExcelFunctionality();
+	boolean valTrue=true;
 	
-	
-	
-	@BeforeMethod
 	public void SetUp() throws InterruptedException, FileNotFoundException, IOException
 	{
-		Log.startTestCase(data.TestCaseDesc+" "+data.TestCaseID);
-		data.getData("TC002", "D:\\ExcelFile\\RumbleOnData.xlsx");
-		System.out.println("Printed Value from data "+data.Browser);
-		System.out.println("Printed Value from data "+data.Environment);
-		System.out.println("Printed Value from data "+data.TestCaseDesc);
-		System.out.println("Printed Value from data "+data.Title);
-		System.out.println("Printed Value from data "+data.URL);
-		System.out.println("Printed Value from data "+data.UserName);
-		System.out.println("Printed Value from data "+data.Password);
-		
-		
 		DOMConfigurator.configure("log4j.xml");
-		
+		Log.startTestCase(data.TestCaseDesc+" "+data.TestCaseID);
+		String MethodName = Thread.currentThread().getStackTrace()[2].getMethodName();
+		System.out.println("Executing: "+MethodName);
+		data.getData(MethodName, "D:\\ExcelFile\\TestData.xlsx");
 		OpenBrowser(data.Browser);
-		captureScreenShot(data.TestCaseID, "");
+		captureScreenShot(data.TestCaseID, MethodName);
 		Log.info("OpeingBrowser:InfoMainTest");
-		Thread.sleep(10000);
 		NavigateToURL(data.URL);
 		captureScreenShot(data.TestCaseID, "");
-		Log.debug("Navigate: From Main test");	
+		Log.debug("Navigate: From Main test");	 
 	}
 	
-	
-	@Test 
-	public void TC001_Login() throws InterruptedException
+	@Test(enabled=true)
+	//TC001_Login
+	public void TC001_Login() throws InterruptedException, FileNotFoundException, IOException
 	{
+		System.out.println("1st");
+		SetUp();
+		LogInPage.LogInWithCredentials(data.UserName, data.Password, data.TestCaseID);
+		
+	}
+	
+	@Test
+	//TC002_Login
+	public void TC002_Login() throws InterruptedException, FileNotFoundException, IOException
+	{
+		System.out.println("2nd");
+		SetUp();
 		LogInPage.LogInWithCredentials(data.UserName, data.Password, data.TestCaseID);
 	}
 	
-	//test
-//	@Test 
-//	public void TC002_SignUp() throws InterruptedException
-//	{
-//		WaitforElementVisisble(By.xpath("//div[@class='sticknav']//a[contains(text(),'SIGN IN / SIGN UP')]"), "Clicking on Sign Up Button", 60).click();
-//		
-//		
-//		WebElement element = driver.findElement(By.xpath("//div[@class='sticknav']//a[contains(text(),'SIGN IN / SIGN UP')]"));
-//		Click(element);
-//		//driver.findElement(By.xpath("//div[@class='sticknav']//a[contains(text(),'SIGN IN / SIGN UP')]")).click();
-//		element = driver.findElement(By.xpath("//p[contains(text(),'Don’t have an account? ')]/span"));
-//		Click(element);
-//		//driver.findElement(By.xpath("//p[contains(text(),'Don’t have an account? ')]/span")).click();
-//		element = driver.findElement(By.xpath("//input[@id='first_name']"));
-//		Highlight(element);
-//		driver.findElement(By.xpath("//input[@id='first_name']")).sendKeys("TestUser1");
-//		driver.findElement(By.xpath("//input[@id='last_name']")).sendKeys("TestLastName");
-//		driver.findElement(By.xpath("//input[@id='phone_number']")).sendKeys("9197490012");
-//		driver.findElement(By.xpath("//input[@id='email_id']")).sendKeys("newuser1234@gmail.com");
-//		driver.findElement(By.xpath("//input[@id='signup_password']")).sendKeys("Pass1234");
-//		driver.findElement(By.xpath("//input[@id='signup_Cpassword']")).sendKeys("Pass1234");
-//		driver.findElement(By.xpath("//input[@id='zip_code']")).sendKeys("75063");
-//		Thread.sleep(20000);
-//		driver.findElement(By.id("btnsignup")).click();
-//		Thread.sleep(20000);
-//		String regText = driver.findElement(By.xpath("//p[@id='pAlertMessage']")).getText();
-//		assertEquals(regText, "You have registered successfully.");
-//		driver.findElement(By.xpath("//div[@id='divAlertPopUp']//button[@id='btnOk']")).click();
-//		Thread.sleep(5000);
-//		driver.findElement(By.xpath("//div[@class='sticknav']//div[@class='profile_pic']")).click();
-//		
-//		
-//		List<WebElement> MyAccountList = driver.findElements(By.xpath("//a[text()='My Account']"));
-//		
-//		for (WebElement webElement : MyAccountList) {
-//			if(webElement.isDisplayed())
-//			{
-//				webElement.click();
-//			}
-//			
-//		}
-//		
-//		Thread.sleep(20000);
-//	}
+	@Test
+	//TC003_Login
+	public void TC003_Login() throws InterruptedException, FileNotFoundException, IOException
+	{
+		System.out.println("2nd");
+		SetUp();
+		LogInPage.LogInWithCredentials(data.UserName, data.Password, data.TestCaseID);
+	}
+	
+	@Test
+	//TC004_Login
+	public void TC004_Login() throws InterruptedException, FileNotFoundException, IOException
+	{
+		System.out.println("2nd");
+		SetUp();
+		LogInPage.LogInWithCredentials(data.UserName, data.Password, data.TestCaseID);
+	}
+	
+	@Test
+	//TC005_Login
+	public void TC005_Login() throws InterruptedException, FileNotFoundException, IOException
+	{
+		System.out.println("2nd");
+		SetUp();
+		LogInPage.LogInWithCredentials(data.UserName, data.Password, data.TestCaseID);
+	}
+	
 	
 	
 	@AfterMethod
--- com.QADeploy/src/main/java/Utilities/ExcelFunctionality.java
+++ com.QADeploy/src/main/java/Utilities/ExcelFunctionality.java
@@ -1,7 +1,5 @@
 package Utilities;
 
-
-
 import java.io.File;
 import java.io.FileInputStream;
 import org.apache.poi.*;
@@ -17,8 +15,8 @@ public class ExcelFunctionality {
 		public String TestCaseID;
 		public String TestCaseDesc;
 		public String Environment;
-		public String Browser;
-		public String URL;
+		public String Browser = null;
+		public String URL =null;
 		public String UserName;
 		public String Password;
 		public String Title;
@@ -26,29 +24,47 @@ public class ExcelFunctionality {
 		public String strDataItem2;
 		public String strDataItem3;
 		public String strDataItem4;
+		public String strDataItem5;
+		public String strDataItem6;
+        public String strDataItem7;
+        public String strDataItem8;
+        public String strDataItem9;
+        public String strDataItem10;
+        public String strDataItem11;
+        public String strDataItem12;
+        public String strDataItem13;
+        public String strDataItem14;
+        public String strDataItem15;
+        public String strDataItem16;
+        public String strDataItem17;
+        public String strDataItem18;
+        public String strDataItem19;
+        public String strDataItem20;
+        public String strDataItem21;
+        public String strDataItem22;
+        public String strDataItem23;
+        public String strDataItem24;
+        public String strDataItem25;
+        public String strDataItem26;
+        public String strDataItem27;
+        public String strDataItem28;
+        public String strDataItem29;
+        public String strDataItem30;
+		
+        
+
 		
 		public void getData(String TestCaseId,String file) throws FileNotFoundException, IOException
 		{
+			File src=new File(file);
+			 FileInputStream fis=new FileInputStream(src);
+			 XSSFWorkbook wb=new XSSFWorkbook(fis);
+			 XSSFSheet sh1= wb.getSheetAt(0);
+			 int rowNo =-1;
+			 int lastRowNo = sh1.getLastRowNum();
+			 String rowData;
 			try {
-				  // Specify the path of file
-				  File src=new File(file);
 				 
-				   // load file
-				   FileInputStream fis=new FileInputStream(src);
-				 
-				   // Load workbook
-				   XSSFWorkbook wb=new XSSFWorkbook(fis);
-				   
-				   // Load sheet- Here we are loading first sheetonly
-				      XSSFSheet sh1= wb.getSheetAt(0);
-				 
-				  // getRow() specify which row we want to read.
-				 
-				  // and getCell() specify which column to read.
-				  // getStringCellValue() specify that we are reading String data.
-				 int rowNo =-1;
-				 int lastRowNo = sh1.getLastRowNum();
-				 String rowData;
 				 for(int i=0; i<lastRowNo;i++)
 				 {
 					 rowData = sh1.getRow(i).getCell(0).getStringCellValue();
@@ -67,19 +83,51 @@ public class ExcelFunctionality {
 					UserName = sh1.getRow(rowNo).getCell(5).getStringCellValue();
 					Password = sh1.getRow(rowNo).getCell(6).getStringCellValue();
 					Title = sh1.getRow(rowNo).getCell(7).getStringCellValue();
+					
+					System.out.println("TestCaseID: "+TestCaseID);
+					System.out.println("TestCaseDesc: "+TestCaseDesc);
+					System.out.println("Environment: "+Environment);
+					System.out.println("Browser: "+Browser);
+					System.out.println("URL: "+URL);
+					System.out.println("UserName: "+UserName);
+					System.out.println("Password: "+Password);
+					System.out.println("Title: "+Title);
+					
 					strDataItem1 = sh1.getRow(rowNo).getCell(8).getStringCellValue();
 					strDataItem2 = sh1.getRow(rowNo).getCell(9).getStringCellValue();
 					strDataItem3 = sh1.getRow(rowNo).getCell(10).getStringCellValue();
 					strDataItem4 = sh1.getRow(rowNo).getCell(11).getStringCellValue();
-				 
-					
-				  } catch (Exception e) {
-				 
-				   System.out.println(e.getMessage());
-				 
+					strDataItem5 = sh1.getRow(rowNo).getCell(12).getStringCellValue();
+					strDataItem6 = sh1.getRow(rowNo).getCell(13).getStringCellValue();
+					strDataItem7 = sh1.getRow(rowNo).getCell(14).getStringCellValue();
+					strDataItem8 = sh1.getRow(rowNo).getCell(15).getStringCellValue();
+					strDataItem9 = sh1.getRow(rowNo).getCell(16).getStringCellValue();
+					strDataItem10= sh1.getRow(rowNo).getCell(17).getStringCellValue();
+					strDataItem11= sh1.getRow(rowNo).getCell(18).getStringCellValue();
+					strDataItem12= sh1.getRow(rowNo).getCell(19).getStringCellValue();
+					strDataItem13= sh1.getRow(rowNo).getCell(20).getStringCellValue();
+					strDataItem14= sh1.getRow(rowNo).getCell(21).getStringCellValue();
+					strDataItem15= sh1.getRow(rowNo).getCell(22).getStringCellValue();
+					strDataItem16= sh1.getRow(rowNo).getCell(23).getStringCellValue();
+					strDataItem17= sh1.getRow(rowNo).getCell(24).getStringCellValue();
+					strDataItem18= sh1.getRow(rowNo).getCell(25).getStringCellValue();
+					strDataItem19= sh1.getRow(rowNo).getCell(26).getStringCellValue();
+					strDataItem20= sh1.getRow(rowNo).getCell(27).getStringCellValue();
+					strDataItem21= sh1.getRow(rowNo).getCell(28).getStringCellValue();
+					strDataItem22= sh1.getRow(rowNo).getCell(29).getStringCellValue();
+					strDataItem23= sh1.getRow(rowNo).getCell(30).getStringCellValue();
+					strDataItem24= sh1.getRow(rowNo).getCell(31).getStringCellValue();
+					strDataItem25= sh1.getRow(rowNo).getCell(32).getStringCellValue();
+					strDataItem26= sh1.getRow(rowNo).getCell(33).getStringCellValue();
+					strDataItem27= sh1.getRow(rowNo).getCell(34).getStringCellValue();
+					strDataItem28= sh1.getRow(rowNo).getCell(35).getStringCellValue();
+					strDataItem29= sh1.getRow(rowNo).getCell(36).getStringCellValue();
+					strDataItem30= sh1.getRow(rowNo).getCell(37).getStringCellValue();
 				  }
 			
-			
+			catch (NullPointerException e)
+			{
+				System.out.println(" ");
+			}
 		}
-	
 }
